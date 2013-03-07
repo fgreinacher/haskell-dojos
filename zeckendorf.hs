ac767a15e8b1aad33dddcbe355e2391b55442141
Florian Greinacher <post@floriangreinacher.de>
2013-03-07 23:00:27 +0100

    Working version of zeckendorf number dojo

--- zeckendorf.hs
+++ zeckendorf.hs
@@ -1,19 +1,48 @@
+import Data.List
 
-zeck :: Int -> [Char]
-zeck n = "test"
+zeckString :: Int -> String
+zeckString n = map (\x -> if(snd x) then '1' else '0') (done $ zeck n)
 
-reverseSmallerFibs :: Int -> [Int]
-reverseSmallerFibs = reverse . smallerFibs
+zeck :: Int -> ZeckState
+zeck n = finalState (ZeckState n (fibsUpTo n) [])
 
-smallerFibs :: Int -> [Int]
-smallerFibs n = takeWhile (\x -> x <= n) fibs
+finalState :: ZeckState -> ZeckState
+finalState state 
+    | nextState' == state    = nextState'
+    | otherwise              = finalState nextState'
+    where
+        nextState' = nextState state
+
+data ZeckState = ZeckState { 
+    rest :: Int, 
+    open :: [Int],
+    done :: [(Int, Bool)] 
+} deriving (Show, Eq)
+
+nextState :: ZeckState -> ZeckState
+nextState state
+    | null open'              = state
+    | (head open') <= rest'   = ZeckState (rest' - (head open')) (tail open') ((done') ++ [((head open'), True)])
+    | otherwise               = ZeckState rest' (tail open') ((done') ++ [((head open'), False)])
+    where 
+        rest' = (rest state)
+        open' = (open state)
+        done' = (done state)
+
+fits :: Int -> Int -> Bool
+fits number limit
+    | number > limit    = False
+    | otherwise         = True
+
+fibsUpTo :: Int -> [Int]
+fibsUpTo upperBound = reverse $ takeWhile (\x -> x <= upperBound) fibs
 
 fibs :: [Int]
-fibs = map nthFib [0..] 
+fibs = map fibAt [0..] 
 
-nthFib :: Int -> Int
-nthFib x 
-    | x < 0     = error "x must no be negative"
-    | x == 0    = 1
-    | x == 1    = 2
-    | otherwise = nthFib (x - 2) + nthFib (x - 1)
+fibAt :: Int -> Int
+fibAt index 
+    | index < 0     = error "index must no be negative"
+    | index == 0    = 1
+    | index == 1    = 2
+    | otherwise     = fibAt (index - 2) + fibAt (index - 1)
