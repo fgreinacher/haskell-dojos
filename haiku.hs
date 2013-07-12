8d6d40ce68168b2a796e1796e324ec8255ed7fe4
Florian Greinacher <post@floriangreinacher.de>
2013-07-12 11:18:14 +0200

    Improved haiku kata

--- haiku.hs
+++ haiku.hs
@@ -1,19 +1,33 @@
+import Data.List.Split
+
+checkHaikus :: String -> [(Int,Int,Int,Bool)]
+checkHaikus input = map checkHaiku $ lines input
 
 checkHaiku :: String -> (Int,Int,Int,Bool)
-checkHaiku input = (0,0,0,False)
+checkHaiku input = if (length lines /= 3) then error "Invalid number of lines" else (syllablesOfLine0,syllablesOfLine1,syllablesOfLine2,isValidHaiku)
+    where
+        lines = splitOn "/" input 
+        syllablesOfLine0 = syllables $ lines !! 0
+        syllablesOfLine1 = syllables $ lines !! 1
+        syllablesOfLine2 = syllables $ lines !! 2
+        isValidHaiku = 
+            syllablesOfLine0 == 5 && 
+            syllablesOfLine1 == 7 && 
+            syllablesOfLine2 == 5
 
-vowels = ['a','e','i','o','u','y']
-
+-- returns whether the given character is a vowel
 isVowel :: Char -> Bool
-isVowel c = elem c vowels
+isVowel = flip elem ['a','e','i','o','u','y']
 
-countSyllables :: String -> Int
-countSyllables s = length $ vowels 
+-- counts the syllables of the given input string
+syllables :: String -> Int
+syllables s = length $ vowels 
     where
         vowels = filter (==True) filteredVowelMap
         filteredVowelMap = filterAdjacentDups vowelMap
         vowelMap = map isVowel s
         
+-- filters adjacent duplicates of the given input list
 filterAdjacentDups :: Eq a => [a] -> [a]
 filterAdjacentDups [] = []
 filterAdjacentDups (x:[]) = [x]
